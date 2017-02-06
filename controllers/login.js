6c8d8ab6b74a77091128306dde144e65f0c5d9ba
Jesse Waddell <jesse@Jesses-MacBook-Pro.local>
2017-02-06 14:08:45 -0600

    added login page and js w/logo working on home page

---
+++ controllers/login.js
@@ -0,0 +1,25 @@
+app.controller('LoginCtrl', function($scope, $location, authFactory) {
+
+// sign in controller
+    $scope.login = () => {
+        console.log('working??')
+
+        authFactory.login($scope.email, $scope.password)
+        .then((response) => {
+            console.log(response)
+            $location.url('/home')
+        })
+    }
+})
+
+// sign in factory
+    .factory('authFactory', ($q) => {
+        return {
+            login(email, pass) {
+                return $q.resolve(firebase.auth().signInWithEmailAndPassword(email, pass))
+            },
+            getUserId() {
+                return firebase.auth().currentUser.uid
+            }
+        }
+    })
