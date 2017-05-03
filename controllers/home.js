09ea252aac9f91070910a8682b1e6d0d132ce893
Jesse Waddell <jesse@Jesses-MacBook-Pro.local>
2017-05-03 10:44:44 -0500

    cleaned up

--- controllers/home.js
+++ controllers/home.js
@@ -5,10 +5,6 @@ app.controller('HomeCtrl', function($scope, $http, $mdToast, $route, authFactory
         console.log(res)
         $scope.res = res
     })
-    // this.items = [];
-    //     for (var i = 0; i < 1000; i++) {
-    //       this.items.push(i);
-    //     }
     $scope.deletePost = function(post, key) {
         console.log(key)
         let delPost = {
@@ -37,7 +33,7 @@ app.controller('HomeCtrl', function($scope, $http, $mdToast, $route, authFactory
 
     $scope.routeGame = function() {
         let gameRoute = {
-            result: $scope.searchGameData,            
+            result: $scope.searchGameData,
             Image: $scope.img,
             comment: $scope.comment,
             uid: authFactory.getUserId(),
@@ -51,17 +47,3 @@ app.controller('HomeCtrl', function($scope, $http, $mdToast, $route, authFactory
     }
 
 })
-
-
-
-// saving posts to home factory
-
-
-// Giant Bomb Api key:
-// http://www.giantbomb.com/api/activate
-// dc75e54a7566ebbd6a99e4edacb2840ae2f6a514
-
-
-// bing api keys:
-// 1. 692e7112434a478d93043f8774f2967b
-// 2. 4a563a6d1c404a23aeee158777d3c485 
--- controllers/search.js
+++ controllers/search.js
@@ -1,53 +1,6 @@
 // homepage navbar js
-// var Nav = angular.module('myApp', ['ngMaterial', 'ngMdIcons']);
 
 app.controller('SearchCtrl', function($scope, $mdSidenav, $http, $mdToast, $route, searchFactory, authFactory) {
-  // $http.defaults.headers.common = '4588299-4a587cc35cb439120e3fe1260'
-  // $http.get(`https://ahmedakhan-game-review-information-v1.p.mashape.com/api/v1/search?game_name=`, {
-  //   headers: {
-  //     'X-Mashape-Key' : 'jfS9AMR1wnmshvZpFflvUUg11Tnpp1iysnujsnLAfEXBSJCk9e'
-  //   }
-  // })
-  //   .then(function(gameData) {
-  //     console.log(gameData.data.result)
-  //     $scope.gameData = gameData.data.result
-  //   })
-
-
-
-// mashape new key: 
-
-  // $http.defaults.headers.common['X-Mashape-Key'] = 'jfS9AMR1wnmshvZpFflvUUg11Tnpp1iysnujsnLAfEXBSJCk9e';
-  // $http.get(`https://ahmedakhan-game-review-information-v1.p.mashape.com/api/v1/information?console=${$scope.search}`)
-  //   // .then(function(gameInfo) {
-    //   console.log(gameInfo)
-    // })
-
-  //   $scope.downVote = function(key, gameRoute) {
-  //   gameRoute.likes = gameRoute.likes - 1
-  // }
-  //   $scope.upVote = function(key, gameRoute) {
-  //   gameRoute.likes = gameRoute.likes + 1
-  // }
-
-    // $scope.routeGame = function() {
-    //     let gameRoute = {
-    //         result: $scope.searchGameData,            
-    //         Image: $scope.img,
-    //         comment: $scope.comment,
-    //         uid: authFactory.getUserId(),
-    //         // likes: 0
-    //     }
-    // console.log(gameRoute)
-    // $http.post('https://gamesole-d397d.firebaseio.com/pin/.json', JSON.stringify(gameRoute))
-    // }
-
-// search bar connected to results
-// $scope.Showme = function() {
-//   let gameObj = {
-//     result: $scope.gameData
-//   }
-// }
 
 $scope.sendCard = function(item) {
   console.log(item)
@@ -72,9 +25,7 @@ $scope.sendCard = function(item) {
   $scope.Showme = function(item) {
       console.log(item, 'item')  
     $http.get(`http://www.giantbomb.com/api/search?api_key=dc75e54a7566ebbd6a99e4edacb2840ae2f6a514&format=json&query="${item}"&resources=game`)
-    // $http.get(`https://ahmedakhan-game-review-information-v1.p.mashape.com/api/v1/search?game_name=${item}`)
     .then(function(search) {
-    // searchFactory.getList()
       console.log(search.data.results)
       $scope.searchGameData = search.data.results
   })
@@ -82,11 +33,10 @@ $scope.sendCard = function(item) {
 };
 
   var vm = this;
-  
   vm.toggleLeft = function() {
     $mdSidenav('left-nav').toggle();
   };
-  
+
   vm.close = function() {
     $mdSidenav('left-nav').close();
   }
